1afe5550657bc40962550d2e525daa8d6beda468
dobroeytro <warm.citrus@gmail.com>
2024-11-03 18:27:13 +0300

    feat: сверстал базово header.

--- components/header/Header.vue
+++ components/header/Header.vue
@@ -1,32 +1,21 @@
 <template>
     <header class="header">
         <div class="header__left">
-            <!-- Иконка фирмы -->
             <a href="/" class="header__logo">
                 <img src="../../public/icons/header_logo.svg" alt="Joki Joya" width="40" height="40" />
             </a>
-
-            <!-- Название города -->
             <div class="header__city" @click="toggleCityMenu">
                 <span>Москва</span>
                 <CityMenu v-if="isCityMenuOpen" @close="toggleCityMenu" />
             </div>
-
-            <!-- Номер телефона -->
             <a href="tel:+78126352277" class="header__phone">+7 (812) 635 22 77</a>
         </div>
-
         <div class="header__right">
-            <!-- Купить билеты -->
             <a href="/buy-tickets" class="header__button header__button--pink">Купить билеты</a>
-
-            <!-- Заказать праздник -->
             <a href="/order-party" class="header__button header__button--green">Заказать праздник</a>
-
-            <!-- Иконка меню -->
             <div class="header__menu-icon" @click="toggleMenu">
                 <img src="../../public/icons/header_menu_burger.svg" alt="Меню" width="40" height="40" />
-                <span>Меню</span>
+                <span class="visually-hidden">Меню</span>
                 <Menu v-if="isMenuOpen" @close="toggleMenu" />
             </div>
         </div>
@@ -34,19 +23,19 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-import CityMenu from './CityMenu.vue'
-import Menu from './Menu.vue'
+import { ref } from "vue";
+import CityMenu from "./CityMenu.vue";
+import Menu from "./Menu.vue";
 
-const isCityMenuOpen = ref(false)
-const isMenuOpen = ref(false)
+const isCityMenuOpen = ref(false);
+const isMenuOpen = ref(false);
 
 function toggleCityMenu() {
-    isCityMenuOpen.value = !isCityMenuOpen.value
+    isCityMenuOpen.value = !isCityMenuOpen.value;
 }
 
 function toggleMenu() {
-    isMenuOpen.value = !isMenuOpen.value
+    isMenuOpen.value = !isMenuOpen.value;
 }
 </script>
 
@@ -57,10 +46,16 @@ function toggleMenu() {
     align-items: center;
     position: sticky;
     top: 0;
-    background-color: white;
+    background-color: rgba(255, 255, 255, 0.75);
     padding: 10px 20px;
-    border-bottom: 2px solid #ccc;
-    /* цвет border укажите в зависимости от макета */
+    border-bottom: 2px solid transparent;
+    border-image: linear-gradient(227.56deg,
+            #fdf398 -0.59%,
+            #f7be87 22.84%,
+            #e5637d 45.63%,
+            #e84bba 65.89%,
+            #9310d8 99.13%);
+    border-image-slice: 1;
 
     &__left,
     &__right {
@@ -74,30 +69,85 @@ function toggleMenu() {
     }
 
     &__city {
-        margin-left: 20px;
-        cursor: pointer;
+        margin-left: 56px;
+        padding: 6.5px 16px;
+        display: flex;
+        align-items: center;
         position: relative;
+        cursor: pointer;
+        gap: 8px;
+        font-family: "DrukCyr";
+        font-weight: 500;
+        color: #5834a4;
+        font-size: 22px;
+        line-height: 24px;
+        letter-spacing: 0.03em;
+        text-transform: uppercase;
+        border: 2px solid #5834a4;
+        border-radius: 30px;
+
+        &::before {
+            content: "";
+            display: block;
+            background-image: url("../../public/icons/header_city.svg");
+            background-repeat: no-repeat;
+            width: 12px;
+            height: 18px;
+        }
     }
 
     &__phone {
-        margin-left: 20px;
+        margin-left: 16px;
+        padding: 10px 16px;
+        display: flex;
+        align-items: center;
+        gap: 8px;
         text-decoration: none;
-        color: inherit;
+        color: #5834a4;
+        font-family: "drukwidecyr";
+        font-size: 13px;
+        font-weight: 500;
+        line-height: 16px;
+        letter-spacing: 0.03em;
+        text-align: center;
+        border: 2px solid #5834a4;
+        border-radius: 30px;
+
+        &::before {
+            content: "";
+            display: block;
+            background-image: url("../../public/icons/header_phone.svg");
+            background-repeat: no-repeat;
+            width: 16px;
+            height: 17px;
+        }
     }
 
     &__button {
-        margin-left: 20px;
-        padding: 10px 15px;
-        border-radius: 5px;
+        padding: 8px 32px;
+        border-radius: 30px;
         text-decoration: none;
-        color: white;
+        text-transform: uppercase;
+        text-align: center;
 
         &--pink {
-            background-color: #ff5a8f;
+            background-color: #f269af;
+            font-family: "DrukCyr";
+            font-size: 22px;
+            font-weight: 500;
+            line-height: 24px;
+            color: #ffffff;
+            letter-spacing: 0.03em;
         }
 
         &--green {
-            background-color: #8bc34a;
+            background-color: #ABEC20;
+            margin-left: 16px;
+            color: #262626;
+            font-family: "DrukCyr";
+            font-size: 22px;
+            font-weight: 500;
+            line-height: 24px;
         }
     }
 
--- components/header/Menu.vue
+++ components/header/Menu.vue
@@ -1,55 +1,64 @@
 <template>
-    <div class="menu-overlay">
-      <nav class="menu">
-        <ul>
-          <li><a href="/about">О нас</a></li>
-          <li><a href="/prices">Цены</a></li>
-          <li><a href="/menu">Меню</a></li>
-          <li><a href="/buy-tickets">Купить билеты</a></li>
-        </ul>
-      </nav>
-    </div>
-  </template>
+  <div class="menu-overlay" @click.self="emit('close')">
+    <nav class="menu">
+      <ul>
+        <li><a href="/about">О нас</a></li>
+        <li><a href="/prices">Цены</a></li>
+        <li><a href="/menu">Меню</a></li>
+        <li><a href="/buy-tickets">Купить билеты</a></li>
+      </ul>
+    </nav>
+  </div>
+</template>
   
-  <script setup lang="ts">
-  import { defineEmits } from 'vue'
+<script setup lang="ts">
+import { defineEmits } from 'vue'
+
+const emit = defineEmits(['close'])
+</script>
   
-  const emit = defineEmits(['close'])
-  </script>
+<style scoped lang="scss">
+.menu-overlay {
+  position: fixed;
+  top: 62px; 
+  left: 0;
+  width: 100%;
+  height: calc(100% - 100px); 
+  background: #ffffff;
+  display: flex;
+  justify-content: flex-end;
+  align-items: flex-start;
+  padding: 20px;
+  z-index: 1000;
+}
+
+.menu {
+  background: none;
+  padding: 0;
   
-  <style scoped lang="scss">
-  .menu-overlay {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: rgba(0, 0, 0, 0.5);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    z-index: 1000;
-  }
-  
-  .menu {
-    background: white;
-    padding: 20px;
-    border-radius: 8px;
-    text-align: center;
-  
-    ul {
-      list-style: none;
-      padding: 0;
-      margin: 0;
-  
-      li {
-        margin: 10px 0;
-  
-        a {
-          text-decoration: none;
-          color: black;
+  ul {
+    list-style: none;
+    padding: 0;
+    
+    li {
+      margin: 20px 0;
+      
+      a {
+        text-decoration: none;
+        font-size: 2rem; 
+        color: #000000;
+        font-family: "drukwidecyr";
+        text-transform: uppercase;
+        display: block;
+        text-align: end;
+        padding-left: 20px;
+        transition: color 0.3s;
+        
+        &:hover {
+          color: #f269af; /* Add hover effect color */
         }
       }
     }
   }
-  </style>
+}
+</style>
